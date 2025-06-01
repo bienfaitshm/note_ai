d2b18dd93e763a1c022f66182c353cd03b4ef2de
bienfait shomari <bienfaitshm@gmail.com>
2025-06-01 12:28:36 +0200

    refactor: améliorer la structure du composant App et supprimer les fichiers CSS inutilisés

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,36 @@
-// import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'node:path'
+// import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 
 // // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//     vueJsx(),
-//     vueDevTools(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     },
-//   },
-// })
-
-
-
-
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), vueJsx(),
-    vueDevTools(),],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+     tailwindcss()
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
+
+
+
+
+// export default defineConfig({
+//   plugins: [vue(), tailwindcss(), vueJsx(),
+//     vueDevTools(),],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// })
